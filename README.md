2183a5e76e0a3bafe21f6e95b6b0f90c2338ab38
Deyverson1 <deyversongp@email.com>
2024-05-03 14:54:34 -0500

    working

---
+++ README.md
@@ -0,0 +1,15 @@
+ clone-repos.py clones all public projects of a specific user.
+
+## Instructions:
+
+1. You must `copy` or `clone` this `repository`
+2. Install the `requests` library and `Python` if you don't already have it.
+>[!WARNING] 
+> Request: <br/>
+> pip installation requests
+
+>[!NOTE]
+>Python Windows:
+https://www.python.org/downloads/
+
+3. Enter the name of the user to whom you want to clone the repositories and a local destination repository where they will be cloned.
---
+++ clone-repos.py
@@ -0,0 +1,14 @@
+import requests #library
+import os
+
+def clone_repositories(username, target_directory):
+    response = requests.get(f"https://api.github.com/users/{username}/repos")
+
+    if response.status_code == 200:
+        for repo in response.json():
+            os.system(f"git clone {repo['clone_url']} {target_directory}/{repo['name']}")
+    else:
+        print("No se pudo obtener la lista de repositorios.")
+
+clone_repositories("Deyverson1",  "C:/Users/Valhalla/Documents/Projects")
+                    #Username and destination directory
